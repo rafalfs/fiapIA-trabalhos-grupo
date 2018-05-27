e5b66d6e36d8b7664be84a841f72729c7ffdc579
BruAndreo <brunolino2026@gmail.com>
2018-05-27 16:39:25 -0300

    Adicionando notebooks de apresentação ao trabalho

---
+++ Trabalho R/R_notebooks/ui.R
@@ -0,0 +1,24 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+
+ui <- navbarPage(
+  "Trabalho R",
+  tabPanel("Grupo", includeHTML("./pages/grupo/groups.nb.html")),
+  tabPanel("Descrição", includeHTML("./pages/descricao/descricao.nb.html")),
+  tabPanel("Enriquecimento do Dataset", includeMarkdown("./pages/enriquecimento/EnriquecimentoDataset.Rmd")),
+  tabPanel("Analises", includeMarkdown("./pages/analises/analises.Rmd")),
+  tabPanel("Subsets"),
+  tabPanel("Analises Graficas"),
+  tabPanel("Modelagem ML")
+)
+
+server <- function(input, output){}
+
+shinyApp(ui = ui, server = server)
